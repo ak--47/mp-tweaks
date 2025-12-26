ab6b06ff0d46ac465656f5e7f45ef8e887bbc510
AK <akrivit@gmail.com>
2025-12-25 22:16:41 -0500

    version

--- src/worker.js
+++ src/worker.js
@@ -66,7 +66,7 @@ function cleanupAllTimers() {
 	activeTimeouts.clear();
 }
 
-const APP_VERSION = `2.49`;
+const APP_VERSION = `2.50`;
 const SCRIPTS = {
 	"hundredX": { path: './src/tweaks/hundredX.js', code: "" },
 	"catchFetch": { path: "./src/tweaks/catchFetch.js", code: "" },
